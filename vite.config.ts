9469e4342a977600b56760a8a54346a364539b70
Veerabhadra Subhash Balla <111112622+scepter99@users.noreply.github.com>
2022-08-31 14:15:22 +0530

    Add rollup visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { crx } from "@crxjs/vite-plugin";
 import { defineConfig } from "vite";
 import manifest from "./manifest.json";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest })]
+  plugins: [react(), crx({ manifest }), visualizer({ template: "sunburst" })]
 });
